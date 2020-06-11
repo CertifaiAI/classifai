cd559cc3c11276deb3e6ba5d76ecb93ddb394989
chiawei <chiawei@skymind.global>
2020-06-11 19:40:09 +0800

    delete old files

--- startIntellibel.bat
+++
@@ -1,8 +0,0 @@
-@echo on
-SET JAR="intellibel-uberjar/target/intellibel-uberjar-1.0-SNAPSHOT-dev.jar"
-call mvn -Puberjar -Dmaven.test.skip=true clean package
-
-set "portnumber=%~2"
-if "%portnumber%"=="" set "portnumber=8080"
-
-java -jar %JAR% --port=%portnumber%
--- startIntellibel.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export JAR="intellibel-uberjar/target/intellibel-uberjar-1.0-SNAPSHOT-dev.jar"
-
-./mvnw -Puberjar -Dmaven.test.skip=true clean package
-java -jar $JAR $1
