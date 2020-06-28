5ef780d3a24790daab2866fdb70624ea77be7e35
chiawei <chiawei@skymind.global>
2020-06-29 04:23:24 +0800

    cleanup

--- startClassifai.sh
+++ startClassifai.sh
@@ -3,8 +3,8 @@
 CLASSIFAI_VERSION="1.0-SNAPSHOT"
 CLASSIFAI_JAR="classifai-uberjar-$CLASSIFAI_VERSION-dev.jar"
 
-export CLASSIFAI_PATH="$HOME/.m2/repository/ai/certifai/classifai-uberjar/$CLASSIFAI_VERSION/$CLASSIFAI_JAR"
+export CLASSIFAI_PATH="$HOME/.m2/repository/ai/certifai/classifai/classifai-uberjar/$CLASSIFAI_VERSION/$CLASSIFAI_JAR"
 
-./mvnw -Puberjar -Dmaven.test.skip=true clean package
+./mvnw -Puberjar -Dmaven.test.skip=true clean install
 
 java -jar $CLASSIFAI_PATH $1 $2
