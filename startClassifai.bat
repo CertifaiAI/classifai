4668570a8b7827f59910302a752e3912bda1a921
chiaweilim <chiawei@skymind.global>
2020-06-29 05:01:54 +0800

    fixed bat script

--- startClassifai.bat
+++ startClassifai.bat
@@ -1,8 +1,5 @@
 @echo on
-SET JAR="classifai-uberjar/target/classifai-uberjar-1.0-SNAPSHOT-dev.jar"
-call mvnw -Puberjar -Dmaven.test.skip=true clean package
+SET JAR="%userprofile%\.m2\repository\ai\certifai\classifai\classifai-uberjar\1.0-SNAPSHOT\classifai-uberjar-1.0-SNAPSHOT-dev.jar"
+call mvnw -Puberjar -Dmaven.test.skip=true clean install
 
-set "portnumber=%~2"
-if "%portnumber%"=="" set "portnumber={port}"
-
-java -jar %JAR% %1 %2
+java -jar %JAR% %*
