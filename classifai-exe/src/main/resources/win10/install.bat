5924a769dfc8732565258438e4d07693210d53f6
chiaweilim <chiawei@skymind.global>
2020-07-20 12:44:44 +0800

    Added exe resources

---
+++ classifai-exe/src/main/resources/win10/install.bat
@@ -0,0 +1,5 @@
+@echo on
+mkdir %APPDATA%\classifai
+move classifai.lnk %USERPROFILE%\Desktop
+move classifai.bat %APPDATA%\classifai
+move classifai-uberjar-1.0-SNAPSHOT-dev.jar %APPDATA%\classifai
