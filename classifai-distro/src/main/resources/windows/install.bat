d85d128167f07ea8067d05d126992554916ad33e
chiawei <chiawei@skymind.global>
2020-08-02 21:23:43 +0800

    renaming classifai-win as classifai-distro

---
+++ classifai-distro/src/main/resources/windows/install.bat
@@ -0,0 +1,58 @@
+@echo off
+
+setlocal
+:PROMPT
+SET /P AREYOUSURE="Continue to install Classifai [Y/N]? "
+IF /I "%AREYOUSURE%" == "N" GOTO ABORT
+IF /I "%AREYOUSURE%" == "Y" GOTO INSTALL
+goto CLOSE
+
+:INSTALL
+rem install classifai program
+mkdir %APPDATA%\classifai
+
+copy classifai.bat %APPDATA%\classifai
+copy classifai-uberjar-1.0-SNAPSHOT-dev.jar %APPDATA%\classifai
+copy classifai_win.ico %APPDATA%\classifai
+
+rem get %desktop%
+
+if exist %USERPROFILE%\Desktop (
+    SET DESKTOP=%USERPROFILE%\Desktop
+rem ) else if exist C:\Users\Admin\OneDrive\Desktop (
+rem    SET DESKTOP=C:\Users\Admin\OneDrive\Desktop
+) else (
+    echo Desktop path not found
+    goto :ABORT
+)
+
+rem create classifai shortcut
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+setx CLASSIFAI_HOME %APPDATA%\classifai
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%DESKTOP%\classifai.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "%APPDATA%\classifai\classifai.bat" >> %SCRIPT%
+echo oLink.IconLocation = "%APPDATA%\classifai\classifai_win.ico" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+
+echo Installation of Classifai completed. Success!
+
+rem start classifai
+endlocal
+goto CLOSE
+
+:ABORT
+echo Installation of Classifai abort.
+echo File an issue on https://github.com/CertifaiAI/classifai
+echo or contact dev team through email helloannotation@certifai.ai.
+goto CLOSE
+
+:CLOSE
+pause
+
