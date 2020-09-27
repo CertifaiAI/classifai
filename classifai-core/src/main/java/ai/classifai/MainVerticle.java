9aae2739c556e1b71abdb55e177aee22465b154e
codenamewei <codenamewei@gmail.com>
2020-09-28 05:21:43 +0800

    wip

--- classifai-core/src/main/java/ai/classifai/MainVerticle.java
+++ classifai-core/src/main/java/ai/classifai/MainVerticle.java
@@ -21,11 +21,13 @@ import ai.classifai.database.portfoliodb.PortfolioVerticle;
 import ai.classifai.database.segdb.SegVerticle;
 import ai.classifai.server.ServerVerticle;
 import ai.classifai.ui.WelcomeConsole;
+import ai.classifai.util.message.LogoLauncher;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * Main verticle to create multiple verticles
@@ -86,6 +88,8 @@ public class MainVerticle extends AbstractVerticle
         }).onComplete(ar ->
         {
             if (ar.succeeded()) {
+
+                LogoLauncher.print();
 
                 WelcomeConsole.start();
 
