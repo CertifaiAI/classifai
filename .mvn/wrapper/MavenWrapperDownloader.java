97c3914f15c0de7b37c4fadcdcba600d5d204e93
toh0510 <72439361+toh-sie-hung@users.noreply.github.com>
2020-10-14 09:55:33 +0800

    updated license header

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-present the original author or authors.
+ * Copyright (c) 2020 CertifAI Sdn. Bhd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
