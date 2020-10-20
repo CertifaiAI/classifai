97f7ba8734b202cce3b50dbfc340bb0ad60a393b
codenamewei <codenamewei@gmail.com>
2020-10-20 13:19:44 +0800

    wip

--- classifai-core/src/main/java/ai/certifai/util/image/PdfHandler.java
+++ classifai-core/src/main/java/ai/certifai/util/image/PdfHandler.java
@@ -72,7 +72,7 @@ public class PdfHandler
         Integer pathLength = pdfFileName.length() - fullPathName.length();
         String pathToSave = pdfFileName.substring(0, pathLength);
 
-        fileName = fileName.replace(".", "_"); //replace any possible "." with "_"
+        //fileName = fileName.replace(".", "_"); //replace any possible "." with "_"
         fileName = fileName.replace(" ", ""); //replace any possible " " with ""
 
         String pathFirstHalf = pathToSave + fileName;
