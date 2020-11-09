0c10ec3d2f103cd959c3f06ca811596a13d2dbbe
codenamewei <codenamewei@gmail.com>
2020-11-09 14:48:29 +0800

    wip

--- classifai-core/src/main/java/ai/classifai/ui/launcher/conversion/Task.java
+++ classifai-core/src/main/java/ai/classifai/ui/launcher/conversion/Task.java
@@ -45,68 +45,68 @@ public class Task extends SwingWorker<Void, Void> {
         if(inputFiles.isEmpty())
         {
             ConverterLauncher.appendTaskOutput("Input file lists empty. Task completed!");
-            return null;
-        }
-
-        String inputFormat = ConverterLauncher.getInputFormat();
-
-        String outputFolderPath = ConverterLauncher.getOutputFolderPath();
-
-        String outputFormat = ConverterLauncher.getOutputFormat();
-
-        int progress = 0;
-        //Initialize progress property.
-        setProgress(progress);
-
-        int fileProcessed = 0;
-
-        if(inputFormat.equals(FileFormat.PDF.getText()))
-        {
-            PdfHandler pdfHandler = new PdfHandler();
-
-            for(File file: inputFiles)
-            {
-                if(isStop) break;
-
-                ConverterLauncher.appendTaskOutput(file.getName());
-
-                String message = pdfHandler.savePdf2Image(file, outputFolderPath, outputFormat);
-
-                if(message != null) ConverterLauncher.appendTaskOutput(message);
-
-                progress = (int) ((++fileProcessed / (double) inputFiles.size()) * 100);
-
-                setProgress(Math.min(progress, 100));
-
-            }
-        }
-        else if(inputFormat.equals(FileFormat.TIF.getText()))
-        {
-            TifHandler tifHandler = new TifHandler();
-
-            for(File file: inputFiles)
-            {
-                if(isStop) break;
-
-                ConverterLauncher.appendTaskOutput(file.getName());
-
-                String message = tifHandler.saveTif2Image(file, outputFolderPath, outputFormat);
-
-                if(message != null) ConverterLauncher.appendTaskOutput(message);
-
-                progress = (int) ((++fileProcessed / (double) inputFiles.size()) * 100);
-
-                setProgress(Math.min(progress, 100));
-
-            }
         }
         else
         {
-            log.info("Input file format not supported: " + inputFormat);
+            String inputFormat = ConverterLauncher.getInputFormat();
 
+            String outputFolderPath = ConverterLauncher.getOutputFolderPath();
+
+            String outputFormat = ConverterLauncher.getOutputFormat();
+
+            int progress = 0;
+            //Initialize progress property.
+            setProgress(progress);
+
+            int fileProcessed = 0;
+
+            if(inputFormat.equals(FileFormat.PDF.getText()))
+            {
+                PdfHandler pdfHandler = new PdfHandler();
+
+                for(File file: inputFiles)
+                {
+                    if(isStop) break;
+
+                    ConverterLauncher.appendTaskOutput(file.getName());
+
+                    String message = pdfHandler.savePdf2Image(file, outputFolderPath, outputFormat);
+
+                    if(message != null) ConverterLauncher.appendTaskOutput(message);
+
+                    progress = (int) ((++fileProcessed / (double) inputFiles.size()) * 100);
+
+                    setProgress(Math.min(progress, 100));
+
+                }
+            }
+            else if(inputFormat.equals(FileFormat.TIF.getText()))
+            {
+                TifHandler tifHandler = new TifHandler();
+
+                for(File file: inputFiles)
+                {
+                    if(isStop) break;
+
+                    ConverterLauncher.appendTaskOutput(file.getName());
+
+                    String message = tifHandler.saveTif2Image(file, outputFolderPath, outputFormat);
+
+                    if(message != null) ConverterLauncher.appendTaskOutput(message);
+
+                    progress = (int) ((++fileProcessed / (double) inputFiles.size()) * 100);
+
+                    setProgress(Math.min(progress, 100));
+
+                }
+            }
+            else
+            {
+                log.info("Input file format not supported: " + inputFormat);
+
+            }
+            done();
         }
-
-        done();
 
         return null;
     }
