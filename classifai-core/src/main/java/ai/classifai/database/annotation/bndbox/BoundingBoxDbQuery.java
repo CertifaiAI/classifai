4a3574cf3e0da5c337c0f951d17abcd621bcd666
codenamewei <codenamewei@gmail.com>
2020-12-27 09:43:30 +0800

    database type change clob -> varchar(65535)

--- classifai-core/src/main/java/ai/classifai/database/annotation/bndbox/BoundingBoxDbQuery.java
+++ classifai-core/src/main/java/ai/classifai/database/annotation/bndbox/BoundingBoxDbQuery.java
@@ -26,7 +26,7 @@ public class BoundingBoxDbQuery extends AnnotationQuery
 {
     private final static String QUEUE = "boundingbox.queue";
 
-    private final static String CREATE_PROJECT = "create table if not exists Project (uuid integer, project_id integer, img_path varchar(2000), bnd_box clob, img_depth integer, " +
+    private final static String CREATE_PROJECT = "create table if not exists Project (uuid integer, project_id integer, img_path varchar(2000), bnd_box varchar(65535), img_depth integer, " +
             "img_x integer, img_y integer, img_w double, img_h double, file_size integer, img_ori_w integer, img_ori_h integer, primary key(uuid, project_id))";
 
     private final static String RETRIEVE_DATA = "select img_path, bnd_box, img_x, img_y, img_w, img_h, file_size, img_ori_w, img_ori_h from Project where uuid = ? and project_id = ?";
