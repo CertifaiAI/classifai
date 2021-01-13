9ae8228cad490e94b87cc1a64126d2ce0a4b3c8a
codenamewei <codenamewei@gmail.com>
2021-01-14 00:53:24 +0800

    modified tests

--- classifai-api/src/test/java/ai/classifai/AppTest.java
+++ classifai-api/src/test/java/ai/classifai/AppTest.java
@@ -4,43 +4,18 @@
 
 package ai.classifai;
 
-import io.vertx.core.Vertx;
-import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
-import io.vertx.ext.web.client.WebClient;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 
 /**
- * Unit test for IntellibelApp
+ * Unit test for ClassifaiApp
  */
 @Slf4j
-@RunWith(VertxUnitRunner.class)
 public class AppTest
 {
-    protected static Vertx vertx;
-    protected static Integer port;
-    protected static WebClient webClient;
-
-    @BeforeClass
-    public static void setUp(TestContext tc) {
-
-
-    }
-
-    @AfterClass
-    public static void testDown(TestContext tc)
-    {
-        //Stop the the Vertx instance and release any resources held by it.
-        vertx.close( r -> {if(r.succeeded()){tc.asyncAssertSuccess();}});
-    }
-
     @Test
-    public void checkStatusCodeSuccess(TestContext context)
+    public void checkStatusCodeSuccess()
     {
 
     }
