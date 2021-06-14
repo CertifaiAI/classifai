ab71877cb1347d9a3587206b5422a2a010d0d5ca
devenyantis <deven.yantis@certifai.ai>
2021-06-14 15:19:13 +0800

    :sparkles: Handle filename exists

--- classifai-core/src/main/java/ai/classifai/action/DeleteProjectData.java
+++ classifai-core/src/main/java/ai/classifai/action/DeleteProjectData.java
@@ -88,6 +88,4 @@ public class DeleteProjectData {
         }
     }
 
-    private static void updateProjectLoader
-
 }
--- classifai-core/src/main/java/ai/classifai/action/FileMover.java
+++ classifai-core/src/main/java/ai/classifai/action/FileMover.java
@@ -1,10 +1,12 @@
 package ai.classifai.action;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -35,10 +37,31 @@ public class FileMover {
     private static void moveFiles(String newDirStr, List<String> filesToMove) throws IOException {
         for(String srcFile: filesToMove) {
             String fName = Paths.get(srcFile).getFileName().toString();
-            log.debug("Move deleted file:\n" + "From: " + Paths.get(srcFile) + "\nTo: " + Paths.get(newDirStr, fName));
-            Files.move(
-                    Paths.get(srcFile),
-                    Paths.get(newDirStr, fName));
+            Path src = Paths.get(srcFile);
+            Path des = Paths.get(newDirStr, fName);
+
+            // Check modify filename if exist
+            if(des.toFile().exists()) {
+                des = getUniqueFilename(newDirStr, des);
+            }
+
+            log.debug("Move deleted file:\n" + "From: " + src + "\nTo: " + des);
+            Files.move(src, des);
         }
+    }
+
+    private static Path getUniqueFilename(String newDirStr, Path des)
+    {
+        int num = 0;
+
+        File desFile = des.toFile();
+        while(desFile.exists()) {
+            String baseName = FilenameUtils.getBaseName(desFile.toString());
+            String ext = FilenameUtils.getExtension(desFile.toString());
+            num++;
+            desFile = Paths.get(newDirStr,baseName + "(" + num + ")" + "." + ext).toFile();
+        }
+
+        return desFile.toPath();
     }
 }
