a7cb6c018857f3730a4775fe779c86155f911b05
devenyantis <deven.yantis@certifai.ai>
2021-07-09 17:12:25 +0800

    :construction: Test server work. Test not working

--- classifai-test/src/test/java/CreateProject.java
+++ classifai-test/src/test/java/CreateProject.java
@@ -1,37 +1,98 @@
+import ai.classifai.MainVerticle;
 import ai.classifai.loader.ProjectLoader;
 import ai.classifai.util.type.AnnotationType;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static io.reactiverse.junit5.web.TestRequest.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
+@ExtendWith({
+        VertxExtension.class,
+})
 public class CreateProject {
     private static ProjectLoader loader = null;
+    String deploymentId;
+
+    @BeforeAll
+    static void prepareDb() throws IOException {
+        DbSetup.backupOriginalDatabase();
+        DbSetup.copyTestDbToUserPath();
+    }
 
     @BeforeEach
-    public void prepareProject() {
-        LoaderSetup.setProjectName("TestProject");
-        LoaderSetup.setAnnotationType(AnnotationType.BOUNDINGBOX.ordinal());
-        LoaderSetup.setTestLabelList(List.of("First", "Second", "Third"));
-        loader = LoaderSetup.createNewTestProject();
+    void setupVerticle(Vertx vertx, VertxTestContext testContext) {
+        testContext.assertComplete(vertx.deployVerticle(new MainVerticle()))
+                .onComplete(ar -> {
+                    deploymentId = ar.result();
+                    testContext.completeNow();
+                });
+    }
+
+    @AfterEach
+    void tearVerticle(Vertx vertx, VertxTestContext testContext) {
+        testContext.assertComplete(vertx.undeploy(deploymentId))
+                .onComplete(ar -> testContext.completeNow());
     }
 
     @Test
     @DisplayName("Create project")
-    void testCreateProject() {
-        Assertions.assertEquals(
-                List.of(loader.getProjectName(), loader.getAnnotationType(), loader.getLabelList()),
-                List.of(LoaderSetup.getProjectName(), LoaderSetup.getAnnotationType(), LoaderSetup.getTestLabelList())
-        );
+    void testCreateProject(Vertx vertx, VertxTestContext testContext) throws InterruptedException {
+        HttpClient client = vertx.createHttpClient();
+        client.request(HttpMethod.GET, "/bndbox/projects/meta")
+                .compose(req -> req.send().compose(HttpClientResponse::body))
+                .onComplete(testContext.succeeding(buffer -> testContext.verify(() -> {
+                    log.info("DEVEN: " + buffer.toString());
+                    assertThat("Plop").isEqualTo("Plop");
+                    testContext.completeNow();
+                })));
+        closeVertx(vertx);
     }
 
-    @Test
-    @DisplayName("Create project server up")
-    void testCreateProjectServerUp(Vertx vertx, Vertx testContext) {
-
+    private void closeVertx(Vertx vertx) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        vertx.close(ar -> latch.countDown());
+        assertThat(latch.await(5, TimeUnit.SECONDS)).isTrue();
     }
+
+//    @Test
+//    @DisplayName("Create project")
+//    void testCreateProject() {
+//        // Create project
+//
+//        Assertions.assertEquals(
+//                List.of(loader.getProjectName(), loader.getAnnotationType(), loader.getLabelList()),
+//                List.of(LoaderSetup.getProjectName(), LoaderSetup.getAnnotationType(), LoaderSetup.getTestLabelList())
+//        );
+//    }
+
+//    @Test
+//    @DisplayName("Create project server up")
+//    void testCreateProjectServerUp(Vertx vertx, VertxTestContext testContext) {
+//
+//    }
+
+    //    private void createProjectNoServer() {
+//        LoaderSetup.setProjectName("TestProject");
+//        LoaderSetup.setAnnotationType(AnnotationType.BOUNDINGBOX.ordinal());
+//        LoaderSetup.setTestLabelList(List.of("First", "Second", "Third"));
+//        loader = LoaderSetup.createNewTestProject();
+//    }
 
 }
